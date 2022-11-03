738b74eec4a1754add9b01e1136f57b0a1438197
psmigielski <walek.smigielski@gmail.com>
2022-11-03 23:10:59 +0100

    add unauthorized nav

--- src/views/AppWrapper.tsx
+++ src/views/AppWrapper.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
+import {AuthContext} from "../context/AuthContext";
+import {UnauthorizedNav} from "../components/atoms/UnauthorizedNav";
+import {Nav} from "../components/molecules/Nav";
+
+import "../styles/AppWrapper.css";
 
 export const AppWrapper = () => {
+    const authContext = React.useContext(AuthContext);
     return (
-        <div>
-            <nav>
-                test2
-            </nav>
+        <div className="appWrapper">
+            <nav className="appWrapper__nav">{authContext.isAuthorized ? <Nav /> : <UnauthorizedNav />}</nav>
             <section>
                 <Outlet />
             </section>
