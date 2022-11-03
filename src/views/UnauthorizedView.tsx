4f17e40938eb4031c7d5aa77dec33d09fc740062
psmigielski <walek.smigielski@gmail.com>
2022-11-03 23:34:04 +0100

    add form handling

--- src/views/UnauthorizedView.tsx
+++ src/views/UnauthorizedView.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {AuthContext} from "../context/AuthContext";
+
+type AuthFormValues = {
+    apiKey: string;
+};
 
 export const UnauthorizedView = () => {
+    const authContext = React.useContext(AuthContext);
+    const {register, handleSubmit, formState:{ errors }}= useForm<AuthFormValues>();
+    const onSubmit: SubmitHandler<AuthFormValues> = async ({apiKey}) => {
+        await authContext.authorize(apiKey);
+    };
     return (
         <div>
-            <form>
-                <input />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <input {...register("apiKey", { minLength: 40, maxLength: 40 })}/>
+                </div>
+                <button type="submit">Kontynuuj</button>
             </form>
         </div>
     )
